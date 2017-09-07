2712fc112a24da866c114f6304b581378bb8d991
joel <iamjoel007@gmail.com>
2017-09-08 04:21:20 +0800

    finish 12

---
+++ nodeschool-practice/12-http-uppercaserer.js
@@ -0,0 +1,37 @@
+var port = process.argv[2]
+var filePath = process.argv[3]
+var fs = require('fs')
+
+var http = require('http')
+var server = http.createServer(function(req, res) {
+  console.log(req.method)
+  if(req.method === 'POST') {
+    var body = ''
+    req.on('data', function(chunk) {
+      body += chunk
+    })
+    req.on('end', function() {
+      var data = body.toUpperCase()
+      res.write(data)
+    })
+  }
+  
+})
+server.listen(port)
+
+
+// 用流的方式写比较优雅
+// var http = require('http')
+// var map = require('through2-map')
+
+// var server = http.createServer(function (req, res) {
+//   if (req.method !== 'POST') {
+//     return res.end('send me a POST\n')
+//   }
+
+//   req.pipe(map(function (chunk) {
+//     return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
